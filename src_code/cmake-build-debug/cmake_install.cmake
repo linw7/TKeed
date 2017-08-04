83c173b03403ef2cad94e6a4f07da0384d5c5d99
INX <linw7@mail2.sysu.edu.cn>
2017-08-05 00:34:50 +0800

    修改

--- src_code/cmake-build-debug/cmake_install.cmake
+++ src_code/cmake-build-debug/cmake_install.cmake
@@ -2,7 +2,7 @@
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/src")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/src_code")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
